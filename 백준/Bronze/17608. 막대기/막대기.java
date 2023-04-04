9d2c1a7f46f5b94e84d584318ca808b7059aaad9
jihye1006 <87265951+jihye1006@users.noreply.github.com>
2023-04-04 16:50:52 +0900

    [Bronze II] Title: 막대기, Time: 236 ms, Memory: 22980 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.java
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,19 +10,20 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int[] height = new int[n];
 
-        for(int i = 0; i<height.length; i++){
+        for(int i = 0; i<height.length; i++){ // n만큼 수 입력 받기
             height[i] = Integer.parseInt(br.readLine());
         }
 
         int count = 1;
-        int max = height[height.length-1];
+        int max = height[height.length-1]; //최댓값 설정
 
-        for(int j= height.length-2; j>=0; j--){
-            if(height[j] > max){
+        for(int j= height.length-2; j>=0; j--){ // 입력 방향 반대로 탐색
+            if(height[j] > max){ 
                 count++;
-                max = height[j];
+                max = height[j]; //최댓값 재설정
             }
         }
         System.out.println(count);
+        br.close();
     }
 }
