d59ae3a47e5df951c1d11ce54193c2eb5ebb559e
jihye1006 <87265951+jihye1006@users.noreply.github.com>
2023-04-06 21:33:37 +0900

    [Silver III] Title: 회전하는 큐, Time: 124 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -1,93 +1,57 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // 덱의 사이즈
-        int M = Integer.parseInt(st.nextToken()); // 찾을 원소의 개수
+        LinkedList<Integer> deque = new LinkedList<Integer>();
+        int count = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken()); // 큐의 크기
+        int m = Integer.parseInt(st.nextToken()); // 뽑아내려고 하는 수의 개수
 
-        Deque<Integer> deq = new LinkedList<>(); // 양방향으로 순회 하므로 덱 선언
+        for(int i = 1; i<n+1; i++){ // 큐에 1~n까지 차례대로 넣어주기
+            deque.offer(i);
+        }
+        int[] sel = new int [m]; //뽑으려고 하는 수를 담은 배열
 
-        for (int i = 1; i < N + 1; i++) { // 원소 1부터 N까지 덱에 추가
-            deq.add(i);
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i<m; i++){
+            sel[i] = Integer.parseInt(st.nextToken()); // 뽑을 수 입력 받기
         }
 
-        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<m; i++){
+            int target = deque.indexOf(sel[i]); //뽑으려고 하는 수의 인덱스
+            int half; // 중간값
 
-        int count = 0; // 연산 2와 연산 3의 최소값
-        int count_2 = 0;
-        int count_3 = 1;
-
-        int[] num_location = new int[M]; // 찾을 원소 위치를 담을 배열
-
-        for (int i = 0; i < M; i++) {
-
-
-
-        }
-
-
-
-
-        for (int i = 0; i < M; i++) {
-            num_location[i] = Integer.parseInt(st.nextToken());// 배열에 원소 위치 입력
-
-            if (deq.peek() == num_location[i]) { // 덱의 맨 앞의 값이 찾을 원소의 위치 라면
-                deq.remove(); // 연산 1 == 첫번째 원소를 뽑아냄
+            if(deque.size() % 2 ==0){ // 짝수일 때
+                half = (deque.size() / 2) -1;
             }
-            else {
-                Iterator<Integer> it_2 = deq.iterator(); // 덱을 순회 하기 위해 Iterator 이용
-                while (it_2.hasNext()) { //
-                    if(it_2.next() == num_location[i]){
-                        break;
-                    }
-                    else{
-                        count_2++;
-                    }
-                }
-                Iterator<Integer> it_3 = deq.descendingIterator(); // 역순회
-                while (it_3.hasNext()) {
-                    if(it_3.next() == num_location[i]){
-                        break;
-                    }
-                    else{
-                        count_3++;
-                    }
-                }
-
-                if(count_2> count_3){
-                    count += count_3;
-                }
-                else{
-                    count += count_2;
-                }
-
-
-                if (count_2 >= count_3) {
-                    for (int j = 0; j < count_3; j++) {
-                        deq.addFirst(deq.removeLast()); // 연산 2 == 왼쪽으로 한 칸 이동
-                    }
-                    deq.remove();
-                }
-                else {
-                    for (int j = 0; j < count_2; j++) {
-                        deq.addLast(deq.removeFirst()); // 연산 3 == 오른쪽으로 한 칸 이동
-                    }
-                    deq.remove();
-                }
-                count_2 = 0;
-                count_3 = 1;
-
+            else{
+                half = deque.size() / 2;
             }
+
+            if (target <= half){ // 뽑아내려고 하는 수가 중간값보다 작거나 같다면
+                for(int j=0; j<target; j++){ //target 수 만큼
+                    int num = deque.pollFirst(); //첫번째에 있는 값 num에 넣어줌 + 제거+ 리턴
+                    deque.offerLast(num); // num을 맨 뒤에 넣어주기
+                    count ++; // 연산 수 ++
+                }
+            }
+            else{ //뽑아내려고 하는 수가 중간값보다 크다면
+                for(int j=0; j<deque.size()-target; j++){ //(deque크기- 뽑아내려고 하는 수)만큼 반복
+                    int num = deque.pollLast(); // 마지막에 있는 값 num에 넣어줌 + 제거 + 리턴
+                    deque.offerFirst(num); // num을 맨 앞에 넣어주기
+                    count++; // 연산 수 ++
+                }
+            }
+            deque.pollFirst(); // 첫번째에 있는 값 제거 + 리턴 (뽑으려고 하는 수)
+
         }
-        System.out.print(count);
+        System.out.println(count); //출력
+        
     }
 }
-
-
